39d4169702ea3bbe69404e2cf848aef1d40497f9
lankut <108450370+lankut@users.noreply.github.com>
2022-07-07 11:16:03 +0300

    Суммирование чисел

---
+++ examples/example_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Сложение чисел");
+int numberA = 5;
+Console.WriteLine(numberA);
+int numberB = 32;
+Console.WriteLine(numberB);
+int sum = numberA+numberB;
+Console.WriteLine(sum);
