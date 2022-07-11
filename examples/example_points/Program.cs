0b9249dc9aa16e4e1aff88689dfe78d4dcbd838b
lankut <108450370+lankut@users.noreply.github.com>
2022-07-11 13:07:01 +0300

    Выполнена Д.З.

--- examples/example_points/Program.cs
+++ examples/example_points/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Clear();
+﻿/*Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("*");
 
@@ -20,9 +20,9 @@ Console.WriteLine("+");
 
 int x = xa, y = xb; 
 int count = 0;
-while (count<50) 
+while (count<100) 
 {
-    int what =new Random().Next(0,3);
+    int what=new Random().Next(0,3);
     if (what == 0)
     {
        x=(x+xa)/2;
@@ -41,4 +41,76 @@ while (count<50)
 Console.SetCursorPosition(x,y);
 Console.WriteLine("+");
 count=count+1;
+}
+*/
+//Задача №2
+/*
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1>number2)
+   Console.WriteLine("Большее число "+number1+" меньшее число"+number2);
+if (number1<number2)
+   Console.WriteLine("Большее число "+number2+" меньшее число"+number1);
+if (number1==number2)
+   Console.WriteLine("Числа равны");
+*/
+
+//Задача №4
+/*
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (number1>number2)
+   max = number1;   
+else max = number2;
+if (max<number3)
+   max = number3;
+
+Console.WriteLine("Максимальное число из трех цифр: "+max);
+*/
+//Задача 6
+
+/*
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2==0)
+   Console.WriteLine("Да!");
+else 
+   Console.WriteLine("Нет");
+*/
+
+//Задача 8
+
+/*Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<number1; i++)
+{
+   if (i % 2==0) 
+      Console.WriteLine(i);   
+}   
+*/
+
+Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+
+while (start <= number1)
+{
+   if (start % 2==0)
+       Console.WriteLine(start);   
+   start++;
 }
